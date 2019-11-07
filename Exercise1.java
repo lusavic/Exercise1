a4f1977c109ab4dc712b1ddb47bb9da22badd367
lusavic <lsavic@citycollege.sheffield.eu>
2019-11-07 14:16:41 +0200

    uploading Exercise1.java file

---
+++ Exercise1.java
@@ -0,0 +1,30 @@
+public class Exercise1{
+public static void main(String[] args){
+	for (int i=0; i<5; i++){
+		System.out.println("FAZE CLAN!");
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+}
+	
+
+
+
+
+
+
+
+}
